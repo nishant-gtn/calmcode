22b46b809747ed699a08ecb435c6893b99f039c1
Nishant Khadka <nishant.khadka@deerwalk.edu.np>
2024-08-23 13:32:13 +0545

    changes

---
+++ decorators.py
@@ -0,0 +1,34 @@
+# def power(n):
+#     def func(number):
+#         return number**n
+
+#     return func
+
+
+# pow2 = power(3)
+# pow3 = power(2)
+
+# print(pow2(2), pow3(3))
+
+
+import time
+import random
+
+
+def stopwatch(f):
+    def func(*args, **kwargs):
+        tic = time.time()
+        result = f(*args, **kwargs)
+        printf(f"This function took: {time.time() - tic}")
+        return result
+
+    return func
+
+
+def sleep_random(s=1):
+    time.sleep(s + random.random())
+    return "Done!"
+
+
+timed_sleep = stopwatch(sleep_random)
+timed_sleep(3)
